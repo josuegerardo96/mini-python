02190067db48679e0cf26e5b66b7a33231e486f7
Josué Gerardo <44277078+josuegerardo96@users.noreply.github.com>
2022-04-21 15:44:58 -0600

    Se agregan notas del profe

--- AContextual.py
+++ AContextual.py
@@ -10,3 +10,21 @@ class AContextual(miParserVisitor):
 
     def visitDefStatementAST(self, ctx: miParserParser.DefStatementASTContext):
         return super().visitDefStatementAST(ctx)
+
+# meter todos el código, todos los visits
+
+# para leer el token
+    #ctx.IDENTIFIER().symbol
+    #ctx.IDENTIFIER().getSymbol()
+
+#buscar no pide token pide texto
+    # ctx.IDENTIFIER().symbol.text
+    # ctx.IDENTIFIER().symbol.text()
+
+# para reportar errores, necesitamos guardar errores sintacticos, de alcance
+# podríamos guardarlos en una lista, despues de hacer la visita en en el main ver si
+# la lista está vacía o no
+    # ctx.IDENTIFIER().symbol.line
+    # ctx.IDENTIFIER().symbol.column
+
+# openScope insertar 
--- TablaSimbolos.py
+++ TablaSimbolos.py
@@ -7,56 +7,47 @@ from antlr4.error.ErrorListener import ErrorListener
 
 
 class TablaSimbolos:
-    tabla= []
-    nivelActual=0
+#    tabla= []
+#    nivelActual=0
 
-
-class Ident:
-    tok=None #Tipo token
-    nivel=0
-    valor=0
-    params=[]
-    isMethod=False
-    declCtx= None #Tipo ParserRuleContext
-
-    def __init__(self, t,  p, im, decl): # t tipo Token, tp tipo int, p tipo lista de Parser1.IdDeclarationContext, im tipo booleano, decl tipo ParserRuleContext
-        self.tok=t
-        nivel= self.nivelActual  ###Unresolved reference 'nivelActual'
-        self.valor=0
-        self.params=p
-        self.isMethod=im
-        self.declCtx=decl
-
-
-    def setValue(self,v):
-        self.valor=v
-
-    def getNivel(self):
-        return self.nivel
+    class Ident(self):  #tal vez poner slef.toko
+        tok:Token
+        def __init__(self, t,  p, im, decl, nivelActual): # t tipo Token, tp tipo int, p tipo lista de Parser1.IdDeclarationContext, im tipo booleano, decl tipo ParserRuleContext
+            self.tok=t
+            # este nivel actual no va a servir
+            self.nivel = nivelActual  ###Unresolved reference 'nivelActual'
+            self.valor=0
+            self.params=p
+            self.isMethod=im
+            self.declCtx=decl
 
     def __int__(self):
-        tabla=[]
+        self.tabla=[]
         self.nivelActual=-1
 
     def insertar(self,id,p,im,decl): # id tipo Token, p tipo lista de Parser1.IdDeclarationContext, im tipo booleano, decl tipo ParserRuleContext
-        self.i= Ident(id,p,im,decl)
-        p.insert(0,self.i)
+        i = Ident(id,p,im,decl, self.nivelActual)
+        p.insert(0,i)
 
     def buscar(self, nombre):
-        temp = None  # temp es tipo Ident
         for i in self.tabla:
-            if i.__eq__(nombre):       #como hacer casting -> (((Ident)id).tok.getText().equals(nombre))
-                return i
-        return temp
+            if i.tok.text() == nombre:       #como hacer casting -> (((Ident)id).tok.getText().equals(nombre))
+                return i.tok
+        return None
 
     def openScope(self):
         self.nivelActual= self.nivelActual+1
 
+    # version que el profe nos pasó, adaptarlo a nuestro proyecto
     def closeScope(self):
-        for i in self.tabla:
-            if i.self.nivel==i.self.nivelActual:
-                self.nivelActual=self.nivelActual-1
+        self.tabla = list(filter(lambda x: x.nivel != self.nivelActual, self.tabla))
+        self.nivelActual -= 1
 
-    def imprimir(self):
-        print("----- INICIO TABLA ------")
-        #for(i in self.tabla):
+    # esta es la función imprimir, se invoca print( str(la_tabla) )
+    def __str__(self):
+        str_list = []
+        str_list.append("----- INICIO TABLA ------\n")
+        for x in self.tabla:
+            str_list.append(f'Nombre: {x.tok.text} - {x.nivel}\n')
+        str_list.append("----- FIN TABLA ------\n")
+        return ''.join(str_list)
